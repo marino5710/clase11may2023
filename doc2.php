0d029bba093fd68e081c5ad2e02af4e5738606f2
marino5710 <brianmarin4@gmail.com>
2023-05-15 08:25:54 -0600

    clase padre herencia

---
+++ doc2.php
@@ -0,0 +1,47 @@
+<?php
+
+class Transporte {
+    public $ruedas; 
+    public $capacidad;
+
+    public function __construct($ruedas, $capacidad)
+    {
+        $this->ruedas = $ruedas; 
+        $this->capacidad = $capacidad;
+    }
+
+    public function getInfo() : void {
+        echo "El transporte tiene $this->ruedas ruedas, y tiene capacidad de: $this->capacidad"; 
+    }
+
+    public function getRuedas() : void {
+        echo "El transporte tiene $this->ruedas ruedas"; 
+    }
+}
+$transporte1 = new Transporte(3,5); 
+
+
+class Carro extends Transporte{
+    public $puertas;
+
+    public function getInfo(): void
+    {
+        echo "El carro tiene $this->ruedas ruedas, y tiene capacidad de: $this->capacidad y tiene $this->puertas puertas";
+    }
+    
+}
+
+$carroToyota = new Carro(4,4); 
+$carroToyota->puertas=2;
+$carroToyota->getInfo();
+
+class Bicicleta extends Transporte {
+    public function getInfo(): void
+    {
+        echo "La bicicleta tiene $this->ruedas ruedas, y tiene capacidad de: $this->capacidad";
+    }
+
+}
+
+$bicicleta = new Bicicleta(2,1); 
+$bicicleta->getInfo();
